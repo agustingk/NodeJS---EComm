9dff4d1373f12ce68d3f87ba7265ff6606d5c548
agustingk <agustingk@icloud.com>
2025-04-28 11:02:32 -0300

    gets all orders

--- api/routes/Order.js
+++ api/routes/Order.js
@@ -26,4 +26,50 @@ orderRoute.post("/", protect, asyncHandler(async (req, res) => {
     }
 }))
 
+///order payment
+orderRoute.put("/:id/payment", protect, asyncHandler(async(req,res)=>{
+    const order = await Order.findById(req.params.id);
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status:req.body.status,
+            update_time:req.body.updated_time,
+            email_address:req.body.email_address,
+        }
+        const updatedOrder = await order.save();
+        res.status(200).json(updatedOrder);
+        res.json(updatedOrder); 
+    }
+    else{
+        res.status(404);
+        throw new Error("Order no encontrado!");
+    }
+}))
+
+//get all orders
+orderRoute.get("/", protect, asyncHandler(async(req, res)=>{
+    const orders = await Order.find({user:req.user._id}).sort({_id:-1})
+    if(orders){
+        res.status(200).json(orders);
+    }
+    else{
+        res.status(404);
+        throw new Error("Order No Encontrado!")
+    }
+}))
+
+//get order by id
+orderRoute.get("/:id", protect, asyncHandler(async(req, res)=>{
+    const order = await Order.findById(req.params.id).populate("user","email");
+    if(order){
+        res.status(200).json(order);
+    }
+    else{
+        res.status(404);
+        throw new Error("Order No Encontrado!")
+    }
+}))
+
 module.exports = orderRoute;
